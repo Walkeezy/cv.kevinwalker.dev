c5aca28ffe4d3e7da0becbb02e4a1c4571de820a
Kevin Walker <mail@kevinwalker.ch>
2021-03-18 14:49:28 +0100

    Fixed google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -10,15 +10,10 @@ class MyDocument extends Document {
     return (
       <Html lang="de">
         <Head>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-MFKYPVMKVC" />
           <script
             dangerouslySetInnerHTML={{
-              __html: `<script async src="https://www.googletagmanager.com/gtag/js?id=G-MFKYPVMKVC"></script>
-                        <script>
-                          window.dataLayer = window.dataLayer || [];
-                          function gtag(){dataLayer.push(arguments);}
-                          gtag('js', new Date());
-                          gtag('config', 'G-MFKYPVMKVC');
-                        </script>`
+              __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-MFKYPVMKVC');`
             }}
           />
         </Head>
